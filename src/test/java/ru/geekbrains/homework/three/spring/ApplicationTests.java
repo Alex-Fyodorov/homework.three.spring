d76f50e401e6926ae710eef1529d9058dfb9bbed
alexf <rassylka82@inbox.ru>
2022-06-09 18:20:06 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/homework/three/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.homework.three.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
